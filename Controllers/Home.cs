9f8c72f4d9730997715eab81a110414f9a6ec51b
Derstine Buyagan <bastedboyderzthine@gmail.com>
2018-11-13 09:23:28 +0800

    Add project files.

---
+++ Controllers/Home.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JavaScript.Controllers
+{
+    public class Home : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
